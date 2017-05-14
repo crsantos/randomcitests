b556498e421b16be295f84796d516d7aa292ab6d
Carlos Santos <carlosricardosantos@gmail.com>
2017-05-14 19:48:48 +0100

    Changed something to trigger CI

--- citests/ViewController.swift
+++ citests/ViewController.swift
@@ -11,15 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.title = "CI Tests"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
